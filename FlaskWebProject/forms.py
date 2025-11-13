eef147ba7cd0b39e18276d18f65127e11f5ab5c2
Suchithabhagavan <117377788+Suchithabhagavan@users.noreply.github.com>
2025-11-13 19:16:03 +0530

    forms.py

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -1,26 +1,19 @@
-# forms.py
+
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
 
-# Login form for manual username/password login
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(max=100)])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-# Form for creating a new article post
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(max=200)])
-    author = StringField('Author', validators=[DataRequired(), Length(max=100)])
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
-    image_path = FileField(
-        'Image',
-        validators=[
-            FileRequired(message="Please upload an image."),
-            FileAllowed(['jpg', 'png'], 'Only .jpg or .png images are allowed!')
-        ]
-    )
-    submit = SubmitField('Save Article')
+    image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Save')
